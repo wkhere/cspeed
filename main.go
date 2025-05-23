938c2c76033e1ac2679acb7d1989a0dea682c9a3
Wojciech Kaczmarek <wojtekk@kofeina.net>
2025-05-23 17:30:19 +0200

    args loop; -d,--duration flag

--- main.go
+++ main.go
@@ -4,26 +4,59 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 type pargs struct {
 	host string
 	dt   time.Duration
+
+	help func()
 }
 
 func defaults() pargs { return pargs{dt: 5 * time.Second} }
 
-func parseArgs(args []string) (p pargs, _ error) {
-	const usage = "usage: cspeed HOST"
+func parseArgs(args []string) (p pargs, err error) {
+	const usage = "usage: cspeed [-d|--duration=DURATION] HOST"
 
 	p = defaults()
 
-	if len(args) != 1 {
-		return p, fmt.Errorf(usage)
+	rest := make([]string, 0, len(args))
+flags:
+	for ; len(args) > 0; args = args[1:] {
+		switch arg := args[0]; {
+
+		case strings.HasPrefix(arg, "-d"), strings.HasPrefix(arg, "--duration"):
+			flag, val, ok := strings.Cut(arg, "=")
+			if !ok {
+				return p, fmt.Errorf("expected %s=DURATION", arg)
+			}
+			p.dt, err = time.ParseDuration(val)
+			if err != nil {
+				return p, fmt.Errorf("flag %s: %w", flag, err)
+			}
+
+		case arg == "-h", arg == "--help":
+			p.help = func() { fmt.Println(usage) }
+			return p, nil
+
+		case arg == "--":
+			rest = append(rest, args[1:]...)
+			break flags
+
+		case len(arg) > 1 && arg[0] == '-':
+			return p, fmt.Errorf("unknown flag %s", arg)
+
+		default:
+			rest = append(rest, arg)
+		}
 	}
 
-	p.host = args[0]
+	if len(rest) != 1 {
+		return p, fmt.Errorf("expecting HOST arg")
+	}
+	p.host = rest[0]
 	return p, nil
 }
 
@@ -33,6 +66,10 @@ func main() {
 	p, err := parseArgs(os.Args[1:])
 	if err != nil {
 		die(2, err)
+	}
+	if p.help != nil {
+		p.help()
+		os.Exit(0)
 	}
 
 	x, err := sshSend(&p)
